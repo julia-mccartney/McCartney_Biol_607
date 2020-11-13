77378a18037b515a8dd7f082ff72ac69cca56536
Julia McCartney <julia.a.mccartney@gmail.com>
2020-11-13 09:13:39 -0500

    last update

---
+++ in_class/2020_11_13_Anovas.R
@@ -0,0 +1,19 @@
+#'------------
+#'
+#' Friday the 13th Lab!
+#' 
+#' 
+#' ------------
+
+
+# Libraries
+library(readr)
+
+
+# Load the data
+kelp  <- read_csv("data/kelp_pred_div_byrnesetal2006.csv")
+bee <- read_csv("data/18q07BeeGeneExpression.csv")
+algae <- read_csv("data/18e3IntertidalAlgae.csv")
+Zooplankton <- read_csv("data/18e2ZooplanktonDepredation.csv")
+
+  
